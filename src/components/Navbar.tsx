40042db8c2bdc99ed52652c5b0e9203b4b85acd3
Win-TS <winthanatpong@gmail.com>
2024-05-18 22:06:22 +0700

    feat: navbar transition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Spin as Hamburger } from 'hamburger-react'
+import { Transition } from '@headlessui/react'
 import logo from '../assets/euroautoparts-logo.png'
 
 export default function Navbar() {
@@ -10,11 +11,11 @@ export default function Navbar() {
     }
 
     return (
-        <nav className='p-4'>
+        <nav className='py-3 px-4 z-50 bg-white'>
             <div className='flex items-center justify-between'>
-                <img src={logo} width={200} />
-                <div className='md:hidden'>
-                    <Hamburger toggled={isMenuOpen} toggle={toggleMenu} size={25}/>
+                <img src={logo} width={200} className='z-50' />
+                <div className='md:hidden z-50'>
+                    <Hamburger toggled={isMenuOpen} toggle={toggleMenu} size={25} />
                 </div>
                 <ul className='hidden md:flex space-x-10'>
                     <li><a href='#' className=''>หน้าหลัก/Home</a></li>
@@ -23,12 +24,22 @@ export default function Navbar() {
                     <li><a href='#' className=''>ติดต่อเรา/Contact</a></li>
                 </ul>
             </div>
-            <ul className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'} flex-col py-2`}>
-                <li className='py-1'><a href='#'>หน้าหลัก/Home</a></li>
-                <li className='py-1'><a href='#'>สินค้า/Products</a></li>
-                <li className='py-1'><a href='#'>คำถามที่พบบ่อย/FAQ</a></li>
-                <li className='py-1'><a href='#'>ติดต่อเรา/Contact</a></li>
-            </ul>
+            <Transition
+                show={isMenuOpen}
+                enter="transition ease-out duration-500 transform"
+                enterFrom="translate-x-full opacity-0"
+                enterTo="translate-x-0 opacity-100"
+                leave="transition ease-in duration-500 transform"
+                leaveFrom="translate-x-0 opacity-100"
+                leaveTo="translate-x-full opacity-0"
+            >
+                <ul className="md:hidden fixed top-0 left-0 w-full bg-gray-500 bg-opacity-90 pt-8 flex flex-col py-2" style={{ top: '77.32px', height: 'calc(100vh - 64px)' }}>
+                    <li className='py-4 text-center'><a href='#'>หน้าหลัก/Home</a></li>
+                    <li className='py-4 text-center'><a href='#'>สินค้า/Products</a></li>
+                    <li className='py-4 text-center'><a href='#'>คำถามที่พบบ่อย/FAQ</a></li>
+                    <li className='py-4 text-center'><a href='#'>ติดต่อเรา/Contact</a></li>
+                </ul>
+            </Transition>
         </nav>
     )
 }
